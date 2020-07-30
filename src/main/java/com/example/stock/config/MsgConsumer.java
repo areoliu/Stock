0a5d405c9bebaa299380dc1ad42f71a4815166a6
areoliu <shuangmianshen@gmail.com>
2020-07-30 18:13:49 +0800

    finish sentinel

--- src/main/java/com/example/stock/config/MsgConsumer.java
+++ src/main/java/com/example/stock/config/MsgConsumer.java
@@ -16,6 +16,7 @@ import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.apache.rocketmq.spring.core.RocketMQPushConsumerLifecycleListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -26,8 +27,12 @@ public class MsgConsumer implements RocketMQListener<MessageExt>, RocketMQPushCo
     @Autowired
     StockServiceImpl stockService;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public void onMessage(MessageExt messageExt) {
+
         String msg = null;
         try{
             msg = new String(messageExt.getBody(),"utf-8");
@@ -36,7 +41,15 @@ public class MsgConsumer implements RocketMQListener<MessageExt>, RocketMQPushCo
             String orderData=JSON.parse(JSONObject.toJSONString(queueMessage.getMsgText())).toString();
             log.info("MsgConsumer 消费者 >>> " +orderData);
             Order order = JSONObject.parseObject(orderData,Order.class);
-            stockService.updateStock(order.getOrderAmout(),order.getOrderSku());
+            String key=""+order.getId();
+            if(stringRedisTemplate.opsForValue().get(key)!=null){
+                stockService.updateStock(order.getOrderAmout(),order.getOrderSku());
+                stringRedisTemplate.delete(key);
+            }else{
+                log.info("MsgConsumer 已消费 >>> " + msg +", msgId = " + messageExt.getMsgId());
+            }
+//            stockService.updateStock(order.getOrderAmout(),order.getOrderSku());
+
         }catch (Exception e){
             e.printStackTrace();
         }
